061849277fc35cbb3fc2f9d10775d38adaf28fc1
rawenfalls <33192916+rawenfalls@users.noreply.github.com>
2022-10-25 00:31:42 +0300

    Delete src directory

--- src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "Zombie.hpp"
-
-int	main(void)
-{
-	Zombie *zombie = newZombie("Buckethead Zombie");
-	randomChump("Basic Zombie");
-	delete zombie;
-	return(0);
-}
--- src/newZombie.cpp
+++
@@ -1,8 +0,0 @@
-#include "Zombie.hpp"
-
-Zombie *newZombie(std::string name)
-{
-	Zombie *buckethead_zombie = new Zombie(name);
-	buckethead_zombie->announce();//or (*buckethead_zombie).announce();
-	return (buckethead_zombie);
-}
